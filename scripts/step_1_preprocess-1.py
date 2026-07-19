871aa7621b691916c773b47d8ab29bed2a034d76
Hussnain-10 <hussnainanser42@gmail.com>
2026-07-19 23:17:02 +0400

    Delete scripts/step_1_preprocess-1.py

--- scripts/step_1_preprocess-1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-def clean_data(path):
-    df=pd.read_csv(path)
-    df=pd.get_dummies(df,columns=['region'],dtype=int,drop_first=True )
-    df['sex']=df['sex'].map({'female':0,'male':1})
-    df['smoker']=df['smoker'].map({'yes':0,'no':1})
-    return df
