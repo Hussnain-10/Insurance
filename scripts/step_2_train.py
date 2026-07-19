0621075b06093ffc500fc41462bbe60140e88690
Hussnain-10 <hussnainanser42@gmail.com>
2026-07-19 13:06:28 -0700

    Update step_2_train.py

--- scripts/step_2_train.py
+++ scripts/step_2_train.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score
-df=clean_data(r'insurance.csv')
+df = clean_data(r'../dataset/insurance.csv')
 X=df.drop('charges',axis=1)
 y=df['charges']
 scaling= MinMaxScaler()
